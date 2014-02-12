66e01b9e061ff2cda1500fd3ffc6dfd6db1e928b
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-02-12 17:32:05 +0400

    Add release task to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,28 @@ module.exports = function(grunt) {
         watch: {
             files: ['**/*.js', '**/*.yate'],
             tasks: ['test']
+        },
+        bump: {
+            options: {
+                commitMessage: 'chore: release v%VERSION%',
+                pushTo: 'origin'
+            }
+        },
+        'npm-publish': {
+            options: {
+                abortIfDirty: true
+            }
+        },
+        'npm-contributors': {
+            options: {
+                commitMessage: 'chore: update contributors'
+            }
         }
     });
 
+    grunt.registerTask('release', 'Bump the version and publish to NPM.', function(type) {
+        return grunt.task.run(['npm-contributors', "bump:" + (type || 'patch') + ':bump-only', 'bump-commit', 'npm-publish']);
+    });
     grunt.registerTask('test', ['karma:unit']);
     grunt.registerTask('default', ['test']);
 };
